34e041435209a2f6e828ef4a8c550dfec8522e22
eniola <opakunleeniolail2019@gmail.com>
2023-08-14 23:29:31 +0100

    Finished the design for the desktop size and the main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const markAsRead = () =>{
+    notificationNo.innerHTML = 0
+    dot1.style.display= "none";
+    dot2.style.display= "none";
+    dot3.style.display= "none";
+    unreadInbox1.style.backgroundColor = 'white';
+    unreadInbox2.style.backgroundColor = 'white';
+    unreadInbox3.style.backgroundColor = 'white';
+  }
