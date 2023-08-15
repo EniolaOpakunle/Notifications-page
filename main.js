351ca1fc7cf4b2ce67750375567a59706ccec6ef
eniola <opakunleeniolail2019@gmail.com>
2023-08-15 17:45:50 +0100

    Used Javascript DOM

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const markAsRead = () =>{
-    notificationNo.innerHTML = 0
-    dot1.style.display= "none";
-    dot2.style.display= "none";
-    dot3.style.display= "none";
-    unreadInbox1.style.backgroundColor = 'white';
-    unreadInbox2.style.backgroundColor = 'white';
-    unreadInbox3.style.backgroundColor = 'white';
-  }
+const markAll = document.getElementById('markAll');
+const notificationNo = document.getElementById('notificationNo')
+let unreadMessages = document.querySelectorAll(".unreadInbox")
+
+notificationNo.innerText = unreadMessages.length;
+
+unreadMessages.forEach((message) => {
+    message.addEventListener("click" , () =>{
+        message.classList.remove("unreadInbox");
+        unreadMessages = document.querySelectorAll(".unreadInbox");
+        notificationNo.innerText = unreadMessages.length;
+    })
+})
+markAll.addEventListener("click", () =>{
+    unreadMessages.forEach((message) =>{
+        message.classList.remove("unreadInbox");
+        unreadMessages = document.querySelectorAll(".unreadInbox")
+        notificationNo.innerText = unreadMessages.length
+    })
+})
